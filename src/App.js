9be219a59759038f07ba0d39e7a51e158d362d33
Elena <estafeliz1102@hotmail.com>
2023-08-22 14:27:28 +0200

    commitElena

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ function App() {
         <p>Hello World, I'm anggi</p>
         <p>Probando, probando, 1, 2, 3</p>
         
+
+        
         {/* <p>parrafo vlad</p> */}
-        {/* <p>parrafo elena</p> */}
+        <p>Hello world, helo Anggi,I'm Elena, nice to meet you</p>
+        <p>Probando, probando, 7, 8, 9</p>
         {/* <p>parrafo milo</p> */}
         {/* <p>parrafo armin</p> */}
         
