dc01b57ef2c43b4fc53c91f897adf44b54b2eeca
omikeflies <omikeflies@gmail.com>
2023-08-23 09:38:35 +0200

    uploading paragraph & img

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
+import './coding.gif'
 
 function App() {
   return (
     <div className="App">
-        <p>Hello World, I'm anggi</p>
-        <p>Probando, probando, 1, 2, 3</p>
-        
-        {/* <p>parrafo vlad</p> */}
-        {/* <p>parrafo elena</p> */}
-        {/* <p>parrafo milo</p> */}
-        <p>Hello World, I'm Armin</p>
-        
+      <p>Hello World, I'm anggi</p>
+      <p>Probando, probando, 1, 2, 3</p>
+
+      {/* <p>parrafo vlad</p> */}
+      {/* <p>parrafo elena</p> */}
+
+      <p>Hello World I'm Miguel👨🏽‍🚀</p>
+      <p>Hello World, I'm Armin</p>
+      <div>
+        <img src="src\coding.gif" alt="Hello world wallpaper" />
+      </div>
     </div>
   );
 }
