31dd0c0d9a8892d0abfb66245655ab92214e4f44
anggifit <abonaldy@gmail.com>
2023-08-23 22:20:25 +0200

    Adjust <div> with image

--- src/App.js
+++ src/App.js
@@ -7,15 +7,14 @@ function App() {
         <p>Probando, probando, 1, 2, 3</p>       
         {/* <p>parrafo vlad</p> */}
         <p>Hello world, hello Anggi, I'm Elena, nice to meet you</p>
-        <p>Probando, probando, 7, 8, 9</p>
-         <p>Hello World I'm Milo👨🏽‍🚀</p>
+        <p>Hello World I'm Milo 👨🏽‍🚀</p>
         <p>Hello World, I'm Armin</p>
-   <div>
-        <img className='img'
-          src="https://hackernoon.com/images/f2px36fy.gif"
-          alt="Hello world wallpaper"
-        />
-      </div>
+        <div>
+          <img className='img'
+            src="https://hackernoon.com/images/f2px36fy.gif"
+            alt="Hello world wallpaper"
+          />
+        </div>
     </div>
   );
 }
