12052f621c371137f9818201cac8ff3c2ef61b86
Elena <estafeliz1102@hotmail.com>
2023-09-01 22:34:17 +0200

    Make change

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-        <p>Hello World, I'm anggi</p>
-        <p>Probando, probando, 1, 2, 3</p>       
-        {/* <p>parrafo vlad</p> */}
-        <p>Hello world, hello Anggi, I'm Elena, nice to meet you</p>
-        <p>Hello World I'm Milo 👨🏽‍🚀</p>
-        <p>Hello World, I'm Armin</p>
-        <div>
-          <img className='img'
-            src="https://hackernoon.com/images/f2px36fy.gif"
-            alt="Hello world wallpaper"
-          />
-        </div>
+      <p>Hello World, I'm anggi</p>
+      <p>Probando, probando, 1, 2, 3</p>
+      {/* <p>parrafo vlad</p> */}
+      <p>Hello world, hello Anggi, I'm Elena, nice to meet you.4,5,6.</p>
+      <p>Hello World I'm Milo 👨🏽‍🚀</p>
+      <p>Hello World, I'm Armin</p>
+      <div>
+        <img
+          className="img"
+          src="https://hackernoon.com/images/f2px36fy.gif"
+          alt="Hello world wallpaper"
+        />
+      </div>
     </div>
   );
 }
